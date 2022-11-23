d972b77477926bd96e9347339d47c204c1d9fd47
zzx <zzx@shangate.com>
2022-11-23 23:38:38 +0800

    add func

--- fg.go
+++ fg.go
@@ -1,8 +1,16 @@
 package goforeground
 
-//Activate attempts to set a window foreground by its process id
-//Linux and Windows are implemented; rest is unimplemented
-//and will return nil
+// Activate attempts to set a window foreground by its process id
+// Linux and Windows are implemented; rest is unimplemented
+// and will return nil
 func Activate(pid int) error {
 	return activate(pid)
+}
+
+func IsForegroundWindow(pid int) (yes bool, err error) {
+	_, e := findWindow(pid)
+	if e != nil {
+		return false, e
+	}
+	return true, nil
 }
